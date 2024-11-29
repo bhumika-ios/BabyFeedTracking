85f32f2b5dfccb7683c3dfb83e362d870e51cbed
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2024-11-29 12:24:51 +0530

    Initial Commit

---
+++ BabyFeedTracking/BabyFeedTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFeedTrackingApp.swift
+//  BabyFeedTracking
+//
+//  Created by Bhumika Patel on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BabyFeedTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyFeedTrackingTests/BabyFeedTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFeedTrackingTests.swift
+//  BabyFeedTrackingTests
+//
+//  Created by Bhumika Patel on 29/11/24.
+//
+
+import Testing
+@testable import BabyFeedTracking
+
+struct BabyFeedTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
